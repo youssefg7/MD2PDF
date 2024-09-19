edf9b0a0a449dcd554a65c876f64eb3b23e12ccd
youssefg7 <67619125+youssefg7@users.noreply.github.com>
2024-09-19 13:43:19 +0300

    Moved to Config File

--- config.py
+++ config.py
@@ -1,19 +1,40 @@
-OUTPUT_DEBUG = True
-OUTPUT_DIR = 'output'
+import os
 
-options = {
-    'page-size': 'A4',
-    'margin-top': '0.75in',
-    'margin-right': '0.75in',
-    'margin-bottom': '0.75in',
-    'margin-left': '0.75in',
-    'encoding': "UTF-8",
-    'no-outline': None
-}
+from pydantic import ConfigDict
+from pydantic_settings import BaseSettings
+from functools import lru_cache
 
-html_template = """
-<html>
-<head>{css_styles}</head>
-<body>{html_content}</body>
-</html>
-"""
+class Settings(BaseSettings):
+    OPENAI_API_KEY: str
+    LLM: str = 'gpt-4o-mini'
+    EMBEDDING_MODEL: str = 'text-embedding-3-small'
+    EMBEDDING_LENGTH: int = 1536
+    
+    OUTPUT_DEBUG: bool = True
+    OUTPUT_DIR: str = 'output'
+    
+    OPTIONS: dict = {
+        'page-size': 'A4',
+        'margin-top': '0.75in',
+        'margin-right': '0.75in',
+        'margin-bottom': '0.75in',
+        'margin-left': '0.75in',
+        'encoding': "UTF-8",
+        'no-outline': None
+    }
+
+    HTML_TEMPLATE: str = """
+    <html>
+    <head>{css_styles}</head>
+    <body>{html_content}</body>
+    </html>
+    """
+
+    model_config = ConfigDict(
+        env_file=".env" if os.getenv("ENVIRONMENT") != "production" else None
+    )
+
+
+@lru_cache()
+def get_settings():
+    return Settings()
--- md2pdf.py
+++ md2pdf.py
@@ -4,9 +4,10 @@ import os
 import markdown2
 import pdfkit
 
-from config import OUTPUT_DEBUG, OUTPUT_DIR, html_template, options
+from config import get_settings
 from utils import auto_direction_html, read_css, read_md, write_html
 
+app_settings = get_settings()
 
 def process_html(html: str) -> str:
     html = auto_direction_html(html)
@@ -19,22 +20,23 @@ if __name__ == "__main__":
     args = parser.parse_args()
     input_md_path = args.input_md_path
 
-    if not os.path.exists(OUTPUT_DIR):
-        os.makedirs(OUTPUT_DIR)
+    if not os.path.exists(app_settings.OUTPUT_DIR):
+        os.makedirs(app_settings.OUTPUT_DIR)
     output_pdf_file = (input_md_path.split("/")[-1]).replace(".md", ".pdf")
-    output_pdf_path = os.path.join(OUTPUT_DIR, output_pdf_file)
+    output_pdf_path = os.path.join(app_settings.OUTPUT_DIR, output_pdf_file)
 
     md = read_md(input_md_path)
     html = markdown2.Markdown(extras=["tables", "fenced-code-blocks"]).convert(md)
     html = process_html(html)
 
-    if OUTPUT_DEBUG:
-        output_html_file = (input_md_path.split("/")[-1]).replace(".md", ".html")
-        output_html_path = os.path.join(OUTPUT_DIR, output_html_file)
-        write_html(html, output_html_path)
 
     css_styles = read_css("styles.css")
-    template = html_template.format(html_content=html, css_styles=css_styles)
+    formatted_template = app_settings.HTML_TEMPLATE.format(html_content=html, css_styles=css_styles)
+    
+    if app_settings.OUTPUT_DEBUG:
+        output_html_file = (input_md_path.split("/")[-1]).replace(".md", ".html")
+        output_html_path = os.path.join(app_settings.OUTPUT_DIR, output_html_file)
+        write_html(formatted_template, output_html_path)
 
-    pdfkit.from_string(template, output_pdf_path, options=options)
+    pdfkit.from_string(formatted_template, output_pdf_path, options=app_settings.OPTIONS)
     print(f"PDF generated at {output_pdf_path}")
